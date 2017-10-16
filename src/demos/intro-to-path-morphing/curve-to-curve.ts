71c039ac822844d8516f2a39f51fbf1c1d429ecc
Alex Lockwood <alexjlockwood@gmail.com>
2017-10-15 22:00:15 -0700

    cleanup

--- src/demos/intro-to-path-morphing/curve-to-curve.ts
+++ src/demos/intro-to-path-morphing/curve-to-curve.ts
@@ -16,7 +16,7 @@ function curveToCurve(shouldMorph: boolean) {
     viewportOptions: { size: 1440, viewportWidth: 24, viewportHeight: 12 },
     fromData: newCurveData([3, 3], [6, 6]),
     toData: newCurveData([13, 1], [18, 6]),
-    hideLabels: shouldMorph,
+    hideLabels: true,
     strokeDashArray: 15,
   });
 }
